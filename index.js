390140831f7da55546ee26b118eee659c5907a1b
Maxim Milovanov <mma29121983@gmail.com>
2018-07-10 17:09:33 -0400

    adds port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 app.use(express.static('public'))
 
-server.listen(process.env.NODE_ENV === 'producation' ? process.env.PORT : 3000);
+server.listen(process.env.PORT || 5000);
 
 let activeTasks = 0;
 let left;
